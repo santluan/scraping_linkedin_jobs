0196f072f08911043de6495cc48cd517129c531c
Luan Santos <luan.santos14@outlook.com>
2025-11-30 15:22:40 -0300

    Add files via upload

---
+++ functions_scrape_linkedin.py
@@ -0,0 +1,118 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+import random
+from unidecode import unidecode
+import re
+
+def get_job_description(job_id):
+    
+    JOB_URL = "https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{}"
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+
+    target_url = JOB_URL.format(job_id)
+    try:
+        response = requests.get(target_url, headers=headers)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+            
+            # The description is usually in a div with this specific class
+            description_box = soup.find("div", class_="show-more-less-html__markup")
+            
+            if description_box:
+                return description_box.get_text(strip=True)
+            else:
+                return "Description not found"
+        else:
+            return f"Error: Status {response.status_code}"
+    except Exception as e:
+        return f"Error: {e}"
+
+def categorize(nivel):
+
+    # Niveis
+    estagio = ['estagio', 'estagiario', 'intern', 'internship']
+    junior = ['junior', 'jr']
+    pleno = ['pleno', 'pl', 'mid']
+    senior = ['senior', 'sr']
+    especialista = ['especialista', 'specialist']
+
+    # tratamento de dados
+    nivel.lower()
+    nivel.replace('iii','sr')
+    nivel.replace('ii','pl')
+    unidecode(string=nivel)
+
+    # Atribuindo o nível a vaga
+    if any(re.search(pattern, nivel, re.IGNORECASE) for pattern in estagio):
+        return "Estágio"
+    elif any(re.search(pattern, nivel, re.IGNORECASE) for pattern in junior):
+        return "Junior"
+    elif any(re.search(pattern, nivel, re.IGNORECASE) for pattern in pleno):
+        return "Pleno"
+    elif any(re.search(pattern, nivel, re.IGNORECASE) for pattern in senior):
+        return "Senior"
+    elif any(re.search(pattern, nivel, re.IGNORECASE) for pattern in especialista):
+        return "Especialista"
+    else:
+        return "Outros"
+
+
+def scrape_linkedin_jobs(keyword, location, start_page, end_page):
+
+    job_list = []
+
+    SEARCH_URL = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search"
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+    
+    print(f"Starting scrape Linkedin Jobs for {keyword} in {location}")
+
+    for page in range(start_page, end_page+1):
+        start_offset = page * 25
+        params = {
+            "keywords": keyword,
+            "location": location,
+            "start": start_offset
+        }
+
+        try:
+            response = requests.get(SEARCH_URL, params=params, headers=headers)
+            
+            if response.status_code != 200:
+                print(f"Scrape failed for page {page+1}. Status code:{response.status_code}")
+                break
+
+            soup = BeautifulSoup(response.text, "html.parser")
+            job_cards = soup.find_all("li")
+            
+            for card in job_cards:
+                title_tag = card.find("h3", class_="base-search-card__title")
+                company_tag = card.find("h4", class_="base-search-card__subtitle")
+                location_tag = card.find("span", class_="job-search-card__location")
+                date_tag = card.find("time", class_="job-search-card__listdate")
+                link_tag = card.find("a", class_="base-card__full-link")
+                jobid_tag = card.find("div", class_="base-card")
+                id = jobid_tag['data-entity-urn'].split(":")[-1]
+
+                job_data = {
+                    "Title": title_tag.text.strip() if title_tag else "NA",
+                    "Company": company_tag.text.strip() if company_tag else "NA",
+                    "Location": location_tag.text.strip() if location_tag else "NA",
+                    "Date_Posted": date_tag['datetime'] if date_tag else "NA",
+                    "Description": get_job_description(id),
+                    "Link": link_tag['href'] if link_tag else "NA",
+                    "Level": categorize(title_tag.text.strip()) if title_tag else "NA"
+                }
+                
+                job_list.append(job_data)
+
+                
+            
+            print(f"Page {page} complete. Total jobs: {len(job_list)}")
+            time.sleep(random.uniform(2, 5))
+
+        except Exception as e:
+            return print(f"Error: {e}")
+    
+    return pd.DataFrame(job_list)  
---
+++ linkedin_dashboard.py
@@ -0,0 +1,107 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+from wordcloud import WordCloud
+from nltk.corpus import stopwords
+from functions_scrape_linkedin import scrape_linkedin_jobs
+
+st.set_page_config(layout="wide")
+
+st.title('📊 Dashboard para Análise de Vagas do Linkedin')
+st.header('Análise de Dados')
+
+# Limpar dados
+def clear_text():
+    st.session_state['my_key'] = ""
+    st.session_state['my_location'] = ""
+
+# Guardando dados em cache
+@st.cache_data
+def scrape_linkedin_cached(k, l):
+    # Deixei 5 páginas por padrão
+    return scrape_linkedin_jobs(k, l, start_page=1, end_page=5)
+    
+#### ---- SIDEBAR ----
+
+with st.sidebar:
+    st.write('Indique abaixo o nome da vaga e localização para iniciar a raspagem')
+
+    keyword = st.text_input("Palavra-chave:", key='my_key')
+    location = st.text_input("Localização:", key='my_location')
+
+    if keyword and location:
+        st.write(f'Começando raspagem de vagas do Linkedin para {keyword} em {location}')
+        st.write('Pode levar um tempo...')
+    
+    st.button('Limpar texto', on_click=clear_text)
+
+#### ---- LÓGICA PRINCIPAL ----
+
+col1, col2 = st.columns(2)
+col3, col4 = st.columns(2)
+
+if keyword and location:
+
+    df = scrape_linkedin_cached(keyword, location)
+    df['Date_Posted'] = pd.to_datetime(df['Date_Posted'], format="%Y-%m-%d")
+    df = df.sort_values(by='Date_Posted')
+
+    with col1:
+        st.write('### Distribuição de vagas por Nível')
+
+        vagas_nivel = df.groupby('Level').size().reset_index()
+        vagas_nivel.columns = ['Nivel', 'Quantidade']
+
+        fig_bar = px.bar(vagas_nivel, x='Nivel', y='Quantidade')
+        st.plotly_chart(fig_bar)
+
+    with col2:
+        st.write('### Nuvem de Palavras das descrições das vagas')
+        
+        col2_1, col2_2 = st.columns(2)
+        with st.container():
+
+            nivel_1 = col2_1.selectbox(
+                "Selecionar nível:",
+                ['Estágio', 'Junior', 'Pleno', 'Senior', 'Especialista', 'Outros'],
+                key='nivel_1'
+                )
+
+            qt_words = col2_2.slider(
+                'Selecione a quantidade de palavras', 
+                10, 
+                500, 
+                100
+                )
+
+        def black_color_func(word, font_size, position,orientation,random_state=None, **kwargs):
+            return("hsl(0,100%, 1%)")
+
+        text = " ".join(description for description in df[df['Level']==nivel_1].Description)
+        stopwords_pt = stopwords.words('portuguese')
+        wordcloud = WordCloud(stopwords=stopwords_pt, background_color="white",max_words=qt_words).generate(text)
+        wordcloud.recolor(color_func=black_color_func)
+        fig, ax = plt.subplots()
+        ax.imshow(wordcloud, interpolation="bilinear")
+        ax.axis("off")
+        st.pyplot(fig)
+
+    st.write('### Tabela de Dados Brutos')
+
+    nivel_2 = st.selectbox(
+        "Selecionar nível:",
+        ['Estágio', 'Junior', 'Pleno', 'Senior', 'Especialista', 'Outros'],
+        key='nivel_2'
+        )
+
+    df = scrape_linkedin_cached(keyword, location)
+    df['Date_Posted'] = pd.to_datetime(df['Date_Posted'], format="%Y-%m-%d")
+    df = df.sort_values(by='Date_Posted')
+    st.dataframe(df[df['Level']==nivel_2], key='my_df')
+
+    
+
+    
+
+
